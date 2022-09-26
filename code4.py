06dc47052dbe031b748a0b9cf73d3e7f65e7ae0b
Jacob Ramey <ramey@tamu.edu>
2022-09-26 15:15:19 -0500

    Made my own branch to edit and make pull requests

--- code4.py
+++ code4.py
@@ -1,57 +1,70 @@
 from urllib.request import urlretrieve
 import os
 import re
-import collections 
+import collections
 
-months_count ={"Jan": 0, "Feb": 0, "Mar": 0, "Apr": 0, "May": 0, "Jun": 0, "Jul": 0, "Aug": 0, "Sep": 0, "Oct": 0, "Nov": 0, "Dec": 0}
+months_count = {"Jan": 0, "Feb": 0, "Mar": 0, "Apr": 0, "May": 0, "Jun": 0, "Jul": 0, "Aug": 0, "Sep": 0, "Oct": 0,
+                "Nov": 0, "Dec": 0}
 
-janlogs=open("january.txt", "a+"); feblogs=open("february.txt", "a+"); marlogs=open("march.txt", "a+"); 
-aprlogs=open("april.txt", "a+"); maylogs=open("may.txt", "a+"); junlogs=open("june.txt", "a+");
-jullogs=open("july.txt", "a+"); auglogs=open("august.txt", "a+"); seplogs=open("september.txt", "a+")
-octlogs=open("octlogs.txt", "a+"); novlogs=open("november.txt", "a+"); declogs=open("december.txt", "a+")   
+janlogs = open("january.txt", "a+")
+feblogs = open("february.txt", "a+")
+marlogs = open("march.txt", "a+")
+aprlogs = open("april.txt", "a+")
+maylogs = open("may.txt", "a+")
+junlogs = open("june.txt", "a+")
+jullogs = open("july.txt", "a+")
+auglogs = open("august.txt", "a+")
+seplogs = open("september.txt", "a+")
+octlogs = open("octlogs.txt", "a+")
+novlogs = open("november.txt", "a+")
+declogs = open("december.txt", "a+")
 
-i=0
+i = 0
 
 redirectCounter = 0
 errorCounter = 0
 URL = 'https://s3.amazonaws.com/tcmg476/http_access_log'
 LOCAL_FILE = 'http_access_log'
 
+
 def file_len(LOCAL_FILE):
-    with open (LOCAL_FILE) as f:
-        for i, l in enumerate (f):
+    with open(LOCAL_FILE) as f:
+        for i, l in enumerate(f):
             pass
     return i + 1
 
+
 def fileCount():
-	filelog = []
-	leastcommon = []
-	with open(LOCAL_FILE) as logs:
-		for line in logs:
-			try:
-				filelog.append(line[line.index("GET")+4:line.index("HTTP")])		
-			except:
-				pass
-	counter = collections.Counter(filelog)
-	for count in counter.most_common(1):														
-		print("Most commonly requested file: {} with {} requests.".format(str(count[0]), str(count[1])))
-	for count in counter.most_common():					
-		if str(count[1]) == '1':
-			leastcommon.append(count[0])
-	if leastcommon:										 													
-		response = input("Looks like there were {} file(s) that were requested only once, show all? (y/n)".format(len(leastcommon)))
-		if response == 'y' or response == 'Y':
-			for file in leastcommon:
-				print(file)
+    filelog = []
+    leastcommon = []
+    with open(LOCAL_FILE) as logs:
+        for line in logs:
+            try:
+                filelog.append(line[line.index("GET") + 4:line.index("HTTP")])
+            except:
+                pass
+    counter = collections.Counter(filelog)
+    for count in counter.most_common(1):
+        print("Most commonly requested file: {} with {} requests.".format(str(count[0]), str(count[1])))
+    for count in counter.most_common():
+        if str(count[1]) == '1':
+            leastcommon.append(count[0])
+    if leastcommon:
+        response = input(
+            "Looks like there were {} file(s) that were requested only once, show all? (y/n)".format(len(leastcommon)))
+        if response == 'y' or response == 'Y':
+            for file in leastcommon:
+                print(file)
+
+
 if not os.path.isfile(LOCAL_FILE):
     urlretrieve(URL, LOCAL_FILE)
-
 
 pattern = r'(.*?) - (.*) \[(.*?)\] \"(.*?) (.*?)\"? (.+?) (.+) (.+)'
 
 lines = open(LOCAL_FILE, 'r').readlines()
 
-#outside loop
+# outside loop
 days_result = []
 date_counter = 0
 current_date = "24/Oct/1994"
@@ -61,64 +74,64 @@ for line in lines:
     if not match:
         continue
 
-    match.group(0) # The original line
-    match.group(3) # The timestamp
+    match.group(0)  # The original line
+    match.group(3)  # The timestamp
     timestamp = match.group(3)
     month = timestamp[3:6]
     months_count[month] += 1
-    match.group(7) # The status codes
-    
-    #inside loop
+    match.group(7)  # The status codes
+
+    # inside loop
     date = timestamp[0:11]
     if date == current_date:
         date_counter += 1
     else:
         days_result.append((current_date, date_counter))
         current_date = date
-        date_counter = 1 
+        date_counter = 1
 
-    if (match.group(7)[0] == "3"):
+    if match.group(7)[0] == "3":
         redirectCounter += 1
-    elif (match.group(7)[0] == "4"):
+    elif match.group(7)[0] == "4":
         errorCounter += 1
-    if (month == "Jan"): 
+    if month == "Jan":
         janlogs.write(line)
-    elif (month == "Feb"): 
+    elif month == "Feb":
         feblogs.write(line)
-    elif (month == "Mar"): 
+    elif month == "Mar":
         marlogs.write(line)
-    elif (month == "Apr"): 
+    elif month == "Apr":
         aprlogs.write(line)
-    elif (month == "May"): 
+    elif month == "May":
         maylogs.write(line)
-    elif (month == "Jun"): 
+    elif month == "Jun":
         junlogs.write(line)
-    elif (month == "Jul"): 
+    elif month == "Jul":
         jullogs.write(line)
-    elif (month == "Aug"): 
+    elif month == "Aug":
         auglogs.write(line)
-    elif (month == "Sep"): 
+    elif month == "Sep":
         seplogs.write(line)
-    elif (month == "Oct"): 
+    elif month == "Oct":
         octlogs.write(line)
-    elif (month == "Nov"): 
+    elif month == "Nov":
         novlogs.write(line)
-    elif (month == "Dec"): 
+    elif month == "Dec":
         declogs.write(line)
-    
+
     else:
         continue
 print("Total requests made:")
 print(file_len(LOCAL_FILE))
 totalResponses = file_len(LOCAL_FILE)
-print("Average requests per day: ", round(totalResponses/365,2))
+print("Average requests per day: ", round(totalResponses / 365, 2))
 for day in days_result:
     print(f"On {day[0]} there were {day[1]} requests")
-print("Average requests per week: ",round(totalResponses/52,2))
-print("Average requests per month:", round(totalResponses/12,2))
+print("Average requests per week: ", round(totalResponses / 52, 2))
+print("Average requests per month:", round(totalResponses / 12, 2))
 print("Month Count:", months_count)
-print("Total number of Errors:",errorCounter)
-print("Percentage of client error (4xx) requests: {0:.2%}".format(errorCounter/totalResponses))	
-print("Total number of redirects:",redirectCounter)
-print("Percentage of all requests that were redirects (3xx): {0:.2%}".format(redirectCounter/totalResponses))
+print("Total number of Errors:", errorCounter)
+print("Percentage of client error (4xx) requests: {0:.2%}".format(errorCounter / totalResponses))
+print("Total number of redirects:", redirectCounter)
+print("Percentage of all requests that were redirects (3xx): {0:.2%}".format(redirectCounter / totalResponses))
 fileCount()
